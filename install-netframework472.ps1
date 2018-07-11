9dd6835e0de95a494d1b4aa37a1f7cd1637abb79
Mathijs de Bruin <debruin@evident.nl>
2018-07-11 17:02:14 +0200

    Add script as ps1 and zip

---
+++ install-netframework472.ps1
@@ -0,0 +1,27 @@
+﻿Configuration NetFrameworkInstall {
+    Import-DscResource -ModuleName PSDesiredStateConfiguration
+
+
+    Node localhost {
+        Script NetFrameworkInstall {
+            GetScript = {
+            }
+
+            SetScript = {
+                $url = "https://download.microsoft.com/download/6/E/4/6E48E8AB-DC00-419E-9704-06DD46E5F81D/NDP472-KB4054530-x86-x64-AllOS-ENU.exe"
+                $filepath = "C:\DSCDeployment\NDP472-KB4054530-x86-x64-AllOS-ENU.exe"
+                Invoke-WebRequest -Uri $url -OutFile $filepath
+                Start-Process -FilePath $filepath -ArgumentList '/q /norestart' -Wait
+            }
+
+            TestScript = {
+                Get-ChildItem 'HKLM:SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full\' |
+                    Get-ItemPropertyValue -Name Release |
+                        ForEach-Object {$_ -ge 461814}
+            }
+        }
+    }
+}
+
+NetFrameworkInstall -OutputPath C:\DSCDeployment
+Start-DscConfiguration -Wait -Path C:\DSCDeployment\ -Force
