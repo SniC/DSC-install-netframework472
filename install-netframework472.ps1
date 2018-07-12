f26b28b01cf6698cb6ea5f065d59d9a33e09ae72
Mathijs de Bruin <debruin@evident.nl>
2018-07-12 16:01:52 +0200

    Set install to passive

--- install-netframework472.ps1
+++ install-netframework472.ps1
@@ -1,7 +1,6 @@
 ﻿Configuration NetFrameworkInstall {
     Import-DscResource -ModuleName PSDesiredStateConfiguration
 
-
     Node localhost {
         Script NetFrameworkInstall {
             GetScript = {
@@ -9,20 +8,24 @@
 
             SetScript = {
                 $url = "https://download.microsoft.com/download/6/E/4/6E48E8AB-DC00-419E-9704-06DD46E5F81D/NDP472-KB4054530-x86-x64-AllOS-ENU.exe"
-                $filepath = "C:\NDP472-KB4054530-x86-x64-AllOS-ENU.exe"
-                Invoke-WebRequest -Uri $url -OutFile $filepath
-                Start-Process -FilePath $filepath -ArgumentList '/norestart' -Wait
+                $output = "C:\NDP472-KB4054530-x86-x64-AllOS-ENU.exe"
+
+                # Download file
+                Invoke-WebRequest -Uri $url -OutFile $output -UseBasicParsing
+
+                # execute file
+                Start-Process -FilePath $output -ArgumentList '/passive /norestart' -Wait
             }
 
             TestScript = {
                 Get-ChildItem 'HKLM:SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full\' |
                     Get-ItemPropertyValue -Name Release |
-                        ForEach-Object {$_ -ge 461815}
+                        ForEach-Object {$_ -ge 461814}
             }
         }
     }
 }
 
 # Uncomment to test on local machine
-NetFrameworkInstall -OutputPath C:\DSCDeployment
-Start-DscConfiguration -Wait -Path C:\DSCDeployment\ -Force
+# NetFrameworkInstall -OutputPath C:\DSCDeployment
+# Start-DscConfiguration -Wait -Path C:\DSCDeployment\ -Force
